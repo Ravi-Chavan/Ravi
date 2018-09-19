c0c85e68665fde5e8b41a9fe3a398b6544df7ffc
Ravi-Chavan <chavanravi@outlook.in>
2018-09-19 22:37:51 +0530

    Initial Commit

---
+++ Sapmple/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Sapmple.Models;
+using System.Web.Mvc;
+
+namespace Sapmple.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>Disply default View</summary>
+        /// <returns>View</returns>
+        public ActionResult Index()
+        {
+            Add model = new Add();
+            return View(model);
+        }
+
+        /// <summary>Addition of two numbers</summary>
+        /// <param name="model">Add</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public ActionResult Index(Add model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Result = model.FirstNumber + model.SecondNumber;
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Sapmple/Models/Add.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sapmple.Models
+{
+    public class Add
+    {
+        [Required]
+        [DisplayName("First Number: ")]
+        public decimal ? FirstNumber  { get; set; }
+
+        [Required]
+        [DisplayName("Second Number: ")]
+        public decimal ? SecondNumber { get; set; }
+    }
+}
